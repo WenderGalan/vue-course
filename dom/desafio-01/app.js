25b5fc378bd0029b597fe3573ddfaf70cc174ebf
Wender <wendergalan2014@hotmail.com>
2020-01-14 21:07:50 -0400

    update...

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Wender Galan',
+        idade: 22,
+        imagem: 'https://www.guiacodigo.com/img/cursos/introducao_ao_vue_js_2/pequena-introducao_ao_vue_js_2_img_93742.png'
+    },
+    methods: {
+    }
+})
