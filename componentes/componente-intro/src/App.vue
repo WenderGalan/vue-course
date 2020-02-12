2643a736a4f7b9912d71b317ebaf342fc3fd7998
Wender <wendergalan2014@hotmail.com>
2020-02-11 23:02:10 -0400

    Update...

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    {{ contador }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
